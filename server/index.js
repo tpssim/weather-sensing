76255d1cd4c52dc564b68aaa7802b0aaf449a662
tpssim <teemu.simola@outlook.com>
2021-04-30 23:05:01 +0300

    Base api functionality

--- server/index.js
+++ server/index.js
@@ -1,8 +1,64 @@
-const express = require('express')
-const app = express()
+const util = require('./util')
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world!<h1>')
+const express = require('express')
+const ULID = require('ulid')
+
+const app = express()
+app.use(express.json())
+
+let readings = [
+    {
+        id: 1,
+        date: "2020-01-10T17:30:31.098Z",
+        sensor: "fake_sensor",
+        data: {
+            temperature: 20.0,
+            pressure: 990.0,
+            humidity: 25.0,
+            ligthlevel: 100.0
+        }
+    },
+    {
+        id: 2,
+        date: "2020-01-10T17:30:31.098Z",
+        sensor: "fake_sensor",
+        data: {
+            temperature: 21.0,
+            pressure: 991.0,
+            humidity: 30.0,
+            ligthlevel: 100.0
+        }
+    }
+]
+
+app.get('/api/readings/:limit', (req, res) => {
+    console.log('Recieved readings get request')
+    const limit = req.params.limit
+    res.json(readings.slice(-limit))
+})
+
+app.post('/api/readings', (req, res) => {
+    console.log('Recieved readings post request')
+    const body = req.body
+
+    try {
+        util.assertReading(body)
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(400).send(err)
+    }
+
+    const reading = {
+        id: ULID.ulid(),
+        date: new Date(),
+        sensor: body.sensor,
+        data: body.data
+    }
+
+    readings = readings.concat(reading)
+
+    return res.json(reading)
 })
 
 const PORT = 3001
---
+++ server/util.js
@@ -0,0 +1,31 @@
+module.exports = {
+    assertReading: (reading) => {
+        if (!reading) {
+            throw 'Invalid request body'
+        }
+
+        const { sensor, data } = reading
+
+        if (!sensor || typeof sensor !== 'string' || sensor.length < 3) {
+            throw 'Invalid or missing parameter "sensor"'
+        }
+        else if (!data) {
+            throw 'Missing parameter data'
+        }
+
+        const { temperature, pressure, humidity, ligthlevel } = data
+
+        if (!temperature || typeof temperature !== 'number') {
+            throw 'Invalid or missing parameter "temperature"'
+        }
+        else if (!pressure || typeof pressure !== 'number') {
+            throw 'Invalid or missing parameter "pressure"'
+        }
+        else if (!humidity || typeof humidity !== 'number') {
+            throw 'Invalid or missing parameter "humidity"'
+        }
+        else if (!ligthlevel || typeof ligthlevel !== 'number') {
+            throw 'Invalid or missing parameter "ligthlevel"'
+        }
+    }
+}
